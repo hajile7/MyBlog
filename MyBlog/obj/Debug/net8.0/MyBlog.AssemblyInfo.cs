21665a1012e650bc5fa0c8093352077ad2f8b85e
Eli <elireid.28@gmail.com>
2024-10-14 16:28:09 -0400

    Almost finished first post, setup routing & CSS classes for code and codeContainer

--- MyBlog/obj/Debug/net8.0/MyBlog.AssemblyInfo.cs
+++ MyBlog/obj/Debug/net8.0/MyBlog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1d555ac10b8c584537d5c22bf0dd934f0309e0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76fe8d722d56d452766e57b5d177b5c05a37be6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
