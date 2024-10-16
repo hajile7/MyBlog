284fdbbb556628208b791f54d1c615e21364e615
Eli <elireid.28@gmail.com>
2024-10-16 15:29:38 -0400

    Ready for publishing!

--- MyBlog/obj/Debug/net8.0/MyBlog.AssemblyInfo.cs
+++ MyBlog/obj/Debug/net8.0/MyBlog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21665a1012e650bc5fa0c8093352077ad2f8b85e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51eaa6eb3a4bf79204c31c1eb217c994f2095fe5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
