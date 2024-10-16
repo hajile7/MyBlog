aa5c01fd1d0da9251d48b2a90128d676bc85c874
Eli <elireid.28@gmail.com>
2024-10-16 16:16:49 -0400

    made build.sh exe

--- MyBlog/obj/Debug/net8.0/MyBlog.AssemblyInfo.cs
+++ MyBlog/obj/Debug/net8.0/MyBlog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+284fdbbb556628208b791f54d1c615e21364e615")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cae32eb81d0aa006f552573cef2ce5436143187d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
