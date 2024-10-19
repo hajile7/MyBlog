1fb0d7bd9f8695deb1da9937b2da00e0cdcc0b4c
Eli <elireid.28@gmail.com>
2024-10-19 12:39:18 -0400

    Fixed padding on mobile, added inline-code class

--- MyBlog/obj/Debug/net8.0/MyBlog.AssemblyInfo.cs
+++ MyBlog/obj/Debug/net8.0/MyBlog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d3074dd48403024049e9c89e5c1e37d391748ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d63fde2e74ff92f05c2c1afd599bde0dfa3758d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
